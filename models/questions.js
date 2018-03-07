0bf30c0ab6d8c750920cd0e5187d5314e907f905
Mia <miacrandell@gmail.com>
2018-03-06 19:54:32 -0700

    add dummy question/answer data

--- models/questions.js
+++ models/questions.js
@@ -1,27 +1,81 @@
 module.exports = function (sequelize, DataTypes) {
-    var Questions = sequelize.define("Questions", {
-        questionId: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        answer: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        date: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        }
-    }, {
-        timestamps: true
-    });
-    Questions.associate = function (models) {
-        Questions.belongsTo(models.UserInfo, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    return Questions;
+	var Questions = sequelize.define("Questions", {
+		questionId: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		answer: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		date: {
+			type: DataTypes.DATEONLY,
+			// allowNull: false
+		}
+	}, {
+		timestamps: true
+	});
+	Questions.sync().then(() => {
+		//first set of question dummy data
+		Questions.create({
+			questionId: 1,
+			answer: "5",
+			UserUserId: 1
+		});
+		Questions.create({
+			questionId: 2,
+			answer: "3",
+			UserUserId: 1
+		});
+		Questions.create({
+			questionId: 3,
+			answer: "2",
+			UserUserId: 1
+		});
+		Questions.create({
+			questionId: 4,
+			answer: "1",
+			UserUserId: 1
+		});
+		Questions.create({
+			questionId: 5,
+			answer: "5",
+			UserUserId: 1
+		});
+		//second set of question dummy data
+		Questions.create({
+			questionId: 1,
+			answer: "4",
+			UserUserId: 2
+		});
+		Questions.create({
+			questionId: 2,
+			answer: "1",
+			UserUserId: 2
+		});
+		Questions.create({
+			questionId: 3,
+			answer: "1",
+			UserUserId: 2
+		});
+		Questions.create({
+			questionId: 4,
+			answer: "1",
+			UserUserId: 2
+		});
+		Questions.create({
+			questionId: 5,
+			answer: "3",
+			UserUserId: 2
+		});
+	});
+	Questions.associate = function (models) {
+		Questions.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
+	return Questions;
 };
