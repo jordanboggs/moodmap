368c9962b2f62dfb768384d752a4a45b11cce508
Jordan Boggs <boggsjp@gmail.com>
2018-03-06 18:27:54 -0700

    tabs

--- models/questions.js
+++ models/questions.js
@@ -1,20 +1,20 @@
 module.exports = function (sequelize, DataTypes) {
-    var Questions = sequelize.define("Question1", {
-        questionId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        answer: {
-            type:  DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    Questions.associate = function (models) {
-        Questions.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    return Questions;
+	var Questions = sequelize.define("Question1", {
+		questionId: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		answer: {
+			type:  DataTypes.STRING,
+			allowNull: false
+		}
+	});
+	Questions.associate = function(models) {
+		Questions.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
+	return Questions;
 };
