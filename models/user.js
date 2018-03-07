f4b079e442cdf59bf70ed017da9bf18d6a0e6204
Jordan Boggs <boggsjp@gmail.com>
2018-03-06 18:02:14 -0700

    DOn't need about section

--- models/user.js
+++ models/user.js
@@ -44,9 +44,6 @@ module.exports = function(sequelize, Sequelize) {
 		username: {
 			type: Sequelize.TEXT
 		},
-		about: {
-			type: Sequelize.TEXT
-		},
 		email: {
 			type: Sequelize.STRING,
 			validate: {
