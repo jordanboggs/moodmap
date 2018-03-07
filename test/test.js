708830378e401252e47c97ef4dbec34817d59714
Mia <miacrandell@gmail.com>
2018-03-07 16:14:47 -0700

    minor adjustments in test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var Nightmare = require("nightmare");
 var expect = require("chai").expect;
 
-
-// module.exports = function () {
     describe("moodMap", function () {
         //setting timeout to 15 sec for now, default is 2
         this.timeout(15000);
@@ -61,55 +59,9 @@ var expect = require("chai").expect;
                 .then(function (title) {
                     expect(title).to.equal('moodMap | Survey');
                     done();
-                });
+                    
+                }).catch(done);
         });
     
     
     });
-// }
-
-// var Nightmare = require("nightmare");
-// var expect = require("chai").expect;
-// describe("Codecademy", function() {
-//   // The default tests in mocha is 2 seconds.
-//   // Extending it to 30 seconds to have time to load the pages
-//   this.timeout(30000);
-//   it("should send user to the catalog page", function(done) {
-//     // ID for the login button.
-//     Nightmare({ show: true })
-//       .goto("https://codecademy.com")
-//       // Click the catalog link
-//       .click("a[href='/learn/all']")
-//       // Evaluate the title
-//       .evaluate(function() {
-//         return document.title;
-//       })
-//       // Asset the title is as expected
-//       .then(function(title) {
-//         expect(title).to.equal("Catalog | Codecademy");
-//         done();
-//       });
-//   });
-//   it("should present a link to course catalog after login", function(done) {
-//     new Nightmare({ show: true })
-//       .goto("https://www.codecademy.com/login")
-//       // Enter username.
-//       .type("#user_login", "ResilD")
-//       // Enter password.
-//       .type("#login__user_password", "dummy*password")
-//       // Click the login button
-//       .click("#user_submit")
-//       // Evaluate the following selector
-//       .evaluate(function() {
-//         // Assert the "learn" link can be found
-//         return document.querySelector("a[href='/learn']");
-//       })
-//       .then(function(link) {
-//         expect(link).to.not.equal(undefined);
-//         done();
-//       });
-//   });
-//   it("should throw an error for fun", function() {
-//     throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
-//   });
-// });
