bb8ecb3f78566c6b0542d810b5003ebd01d4eaec
ColleenM <crawleen@yahoo.com>
2018-03-08 20:04:33 -0700

    userid logic in charts and other fixes

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,3 +1,114 @@
+let userId;
+$.get("/api/userId").then(function (data) {
+    userId = data.userId;
+    console.log("USER ID = " + userId);
+});
+
+var quotes = [{
+    quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
+    name: "Francis of Assisi"
+    },
+    {
+    quote: "Believe you can and you're halfway there.",
+    name: "Theodore Roosevelt"
+    },
+    {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    name: "Confucius"
+    },
+    {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    name: "Thomas A. Edison"
+    },
+    {
+    quote: "The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
+    name: "Confucius"
+    },
+    {
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    name: "Sam Levenson"
+    },
+    {
+    quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+    name: "Ayn Rand"
+    },
+    {
+    quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+    name: "Ayn Rand"
+    },
+    {
+    quote: "Expect problems and eat them for breakfast.",
+    name: "Alfred A. Montapert"
+    },
+    {
+    quote: "Start where you are. Use what you have. Do what you can.",
+    name: "Arthur Ashe"
+    },
+    {
+    quote: "Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
+    name: "Samuel Beckett"
+    },
+    {
+    quote: "Be yourself; everyone else is already taken.",
+    name: "Oscar Wilde"
+    },
+    {
+    quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    name: "Albert Einstein"
+    },
+    {
+    quote: "Always remember that you are absolutely unique. Just like everyone else.",
+    name: "Margaret Mead"
+    },
+    {
+    quote: "Do not take life too seriously. You will never get out of it alive.",
+    name: "Elbert Hubbard"
+    },
+    {
+    quote: "People who think they know everything are a great annoyance to those of us who do.",
+    name: "Isaac Asimov"
+    },
+    {
+    quote: "Procrastination is the art of keeping up with yesterday.",
+    name: "Don Marquis"
+    },
+    {
+    quote: "Get your facts first, then you can distort them as you please.",
+    name: "Mark Twain"
+    },
+    {
+    quote: "A day without sunshine is like, you know, night.",
+    name: "Steve Martin"
+    },
+    {
+    quote: "My grandmother started walking five miles a day when she was sixty. She's ninety-seven now, and we don't know where the hell she is.",
+    name: "Ellen DeGeneres"
+    },
+    {
+    quote: "Don't sweat the petty things and don't pet the sweaty things.",
+    name: "George Carlin"
+    },
+    {
+    quote: "Always do whatever's next.",
+    name: "George Carlin"
+    },
+    {
+    quote: "Hapiness is not something ready made. It comes from your own actions.",
+    name: "Dalai Lama"
+    }
+];
+
+function getQuote() {
+    var randomQuote = Math.floor(Math.random() * quotes.length);
+    var selectedQuote = quotes[randomQuote];
+    $('#main-quote').append(selectedQuote.quote);
+    $('#author').append(selectedQuote.name);
+};
+
+$(function () {
+    getQuote(); // call initially and get random quote
+});
+
 //chart1
 var quest1Data = [];
 var quest1Labels = [];
@@ -5,11 +116,13 @@ var questionId = 1;
 var formattedDate;
 
 $.get("/api/charts/question_id/" + questionId, function(data) {
-    //console.log(data);
+    console.log(data);
     for (var i = 0; i < data.length; i++) {
-        quest1Data.push(data[i].answer);
-        formattedDate = moment(data[i].createdAt).format("dddd");
-        quest1Labels.push(formattedDate);        
+        if(data[i].UserUserId === userId){
+            quest1Data.push(data[i].answer);
+            formattedDate = moment(data[i].createdAt).format("MMM Do YY");
+            quest1Labels.push(formattedDate);
+        }        
     }   
 
     var ctx = document.getElementById("question1").getContext('2d');
@@ -74,9 +187,11 @@ questionId = 2;
 $.get("/api/charts/question_id/" + questionId, function(data) {
     //console.log(data);
     for (var i = 0; i < data.length; i++) {
-        quest2Data.push(data[i].answer);
-        formattedDate = moment(data[i].createdAt).format("MMM Do YY");
-        quest2Labels.push(formattedDate);       
+        if(data[i].UserUserId === userId){
+            quest2Data.push(data[i].answer);
+            formattedDate = moment(data[i].createdAt).format("MMM Do YY");
+            quest2Labels.push(formattedDate);   
+        }    
     }   
 
     var ctx = document.getElementById("question2").getContext('2d');
@@ -137,9 +252,11 @@ questionId = 3;
 $.get("/api/charts/question_id/" + questionId, function(data) {
     //console.log(data);
     for (var i = 0; i < data.length; i++) {
-        quest3Data.push(data[i].answer);
-        formattedDate = moment(data[i].createdAt).format("MMM Do YY");
-        quest3Labels.push(formattedDate);    
+        if(data[i].UserUserId === userId){
+            quest3Data.push(data[i].answer);
+            formattedDate = moment(data[i].createdAt).format("MMM Do YY");
+            quest3Labels.push(formattedDate);   
+        } 
     }   
 
     var ctx = document.getElementById("question3").getContext('2d');
@@ -203,14 +320,16 @@ var noCount = 0;
 $.get("/api/charts/question_id/" + questionId, function(data) {
     //console.log(data);
     for (var i = 0; i < data.length; i++) {
-        if(data[i].answer === '1'){
-            yesCount += 1;
+        if(data[i].UserUserId === userId){
+            if(data[i].answer === '1'){
+                yesCount += 1;
+            }
+            else if(data[i].answer === '0'){
+                noCount += 1;
+            }
+            formattedDate = moment(data[i].createdAt).format("MMM Do YY");
+            quest4Labels.push(formattedDate);   
         }
-        else if(data[i].answer === '0'){
-            noCount += 1;
-        }
-        formattedDate = moment(data[i].createdAt).format("MMM Do YY");
-        quest4Labels.push(formattedDate);   
     }   
 
     var ctx = document.getElementById("question4").getContext('2d');
@@ -269,9 +388,11 @@ questionId = 5;
 $.get("/api/charts/question_id/" + questionId, function(data) {
     //console.log(data);
     for (var i = 0; i < data.length; i++) {
-        quest5data.push(data[i].answer);
-        formattedDate = moment(data[i].createdAt).format("MMM Do YY");
-        quest5Labels.push(formattedDate);   
+        if(data[i].UserUserId === userId){
+            quest5data.push(data[i].answer);
+            formattedDate = moment(data[i].createdAt).format("MMM Do YY");
+            quest5Labels.push(formattedDate);   
+        }
     }   
 
     var ctx = document.getElementById("question5").getContext('2d');
