d14de317f25e9092501766a91406381b058762ac
Jordan Boggs <boggsjp@gmail.com>
2018-03-09 10:09:29 -0700

    Fixed issue where UserId was submitted as question answer

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -7,18 +7,19 @@ module.exports = function (app) {
         console.log('surveys route hit');
 
         var answersArray = Object.values(req.body);
-        console.log(answersArray);
+        console.log("answersArray",answersArray);
         // Loop through Questions 1-5
         var bulkArray = [];
         for (let i = 1; i <= 5; i++) {
             console.log("questionId:",i);
-            console.log("answer:",answersArray[i-1]); // -1 because arrays start at 0
+            console.log("answer:",answersArray[i]); // -1 because arrays start at 0
             bulkArray.push({
                 questionId: i,
-                answer: answersArray[i-1],
+                answer: answersArray[i],
                 UserUserId: answersArray[0] // hard code for now
             });
         }
+        console.log("bulkArray",bulkArray);
         db.Questions.bulkCreate(bulkArray).then(function (dbQuestions) {
             res.json(dbQuestions);
         });
